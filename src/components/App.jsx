2033260d68f96902d721662a5e98d1c1bceb3009
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-08-04 12:08:40 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,12 @@ import AdalabProjects from "./AdalabProjects";
 import { useState, useEffect } from "react";
 
 
+
+
 function App() {
   const [projectsAda, setProjectsAda] = useState([]);
   useEffect(() => {
-    fetch("/Adalab.json")
+    fetch("Adalab.json")
       .then(response => response.json())
       .then(data => setProjectsAda(data))
       .catch((error) => console.error("Error loading projects", error));
@@ -20,7 +22,7 @@ function App() {
 
   const [projectsOwn, setProjectsOwn] = useState([]);
   useEffect(() => {
-    fetch("/Own.json")
+    fetch("Own.json")
       .then(response => response.json())
       .then(data => setProjectsOwn(data))
       .catch((error) => console.error("Error loading projects", error));
